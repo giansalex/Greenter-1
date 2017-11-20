f0543207d958abb41b3c5999be21bf84c4eb24a3
sienrik15 <enrique_sam11@hotmail.com>
2017-11-20 05:09:52 -0500

    se añadio tabs comprobantes de pago

---
+++ app/src/main/java/com/analycer/greenter/greenter/fragments/ComprobantesFragment.java
@@ -0,0 +1,95 @@
+package com.analycer.greenter.greenter.fragments;
+
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.analycer.greenter.greenter.R;
+import com.analycer.greenter.greenter.fragments.tabs.BoletasFragment;
+import com.analycer.greenter.greenter.fragments.tabs.FacturasFragment;
+import com.analycer.greenter.greenter.fragments.tabs.NotaCreditoFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ComprobantesFragment extends Fragment {
+
+
+    private TabLayout tabs;
+    private ViewPager viewPager;
+
+    public ComprobantesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_comprobantes, container, false);
+        // Setting ViewPager for each Tabs
+        viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        // Set Tabs inside Toolbar
+        tabs = (TabLayout) view.findViewById(R.id.result_tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        return view;
+    }
+
+    // Add Fragments to Tabs
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getChildFragmentManager());
+        adapter.addFragment(new FacturasFragment(), "Fact");
+        adapter.addFragment(new BoletasFragment(), "Bolet");
+        adapter.addFragment(new NotaCreditoFragment(), "NotCre");
+        viewPager.setAdapter(adapter);
+    }
+
+    /**adaptador de fragmentos*/
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+}
